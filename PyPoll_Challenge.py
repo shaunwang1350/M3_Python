eadb8e55b7825a2cb318da9d62f2db1ef3bb83cc
Owangatang <shaunwangg@gmail.com>
2020-06-06 17:15:46 -0400

    Finished Challenge, working on further refactoring

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -7,12 +7,18 @@ file_to_save = os.path.join('Analysis',"election_analysis.txt")
 total_votes = 0
 candidate_options = []
 candidate_votes = {}
+county_options = []
+county_votes = {}
 
 winning_candidate = ""
 winning_count = 0
 winnning_percentage = 0
 
-dashs = f"------------------------------\n"
+winning_county = ""
+county_Wcount = 0
+county_Wpercentage = 0
+
+dashs = f"--------------------------------------------\n"
 
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
@@ -22,12 +28,19 @@ with open(file_to_load) as election_data:
     for row in file_reader:
         total_votes += 1
         candidate_name = row[2]
+        county_name = row[1]
 
         if candidate_name not in candidate_options:
             candidate_options.append(candidate_name)
             candidate_votes[candidate_name] = 0
 
         candidate_votes[candidate_name] += 1
+
+        if county_name not in county_options:
+            county_options.append(county_name)
+            county_votes[county_name] = 0
+
+        county_votes[county_name] += 1
 
 with open(file_to_save, "w") as txt_file:
 
@@ -36,9 +49,31 @@ with open(file_to_save, "w") as txt_file:
         f"{dashs}"
         f"Total Votes: {total_votes:,}\n"
         f"{dashs}"
+        f"County Votes: \n"
     )
     print(election_results, end="")
     txt_file.write(election_results)
+
+    
+
+    for county in county_votes:
+        c_votes = county_votes[county]
+        c_vote_percentage = (int(c_votes)/int(total_votes))*100
+
+        county_results = (f"{county}: {c_vote_percentage:.1f}% ({c_votes:,})\n")
+        txt_file.write(county_results)
+
+        if c_votes > county_Wcount and c_vote_percentage > county_Wpercentage:
+            winning_county, county_Wcount, county_Wpercentage = county, c_votes, c_vote_percentage
+
+    county_winning_summary = (
+        f"{dashs}"
+        f"Largest County Turnout: {winning_county} with {county_Wpercentage:.1f}% ({county_Wcount:,})\n"
+        f"{dashs}"
+    )
+
+    print(county_winning_summary)
+    txt_file.write(county_winning_summary)
 
     for candidate in candidate_votes:
         votes = candidate_votes[candidate]
@@ -47,16 +82,17 @@ with open(file_to_save, "w") as txt_file:
         if votes > winning_count and vote_percentage > winnning_percentage:
             winning_candidate, winning_count, winnning_percentage = candidate, votes, vote_percentage
 
-        candidate_results = (f"{candidate}: {vote_percentage:.2f}% ({votes:,})\n")
+        candidate_results = (f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n")
         txt_file.write(candidate_results)
 
-    winning_summary = (
+    candidate_winning_summary = (
+        
         f"{dashs}"
         f"Winner: {winning_candidate}\n"
         f"Winning vote count: {winning_count:,}\n"
-        f"Winning percentage: {winnning_percentage:.2f}% \n"
+        f"Winning percentage: {winnning_percentage:.1f}% \n"
         f"{dashs}"
     )
 
-    print(winning_summary)
-    txt_file.write(winning_summary)
+    print(candidate_winning_summary)
+    txt_file.write(candidate_winning_summary)
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# M3_Python
+# M3_Python Challenge
+## Objectives
+
+* Calculate the oter turnout for each county as well as the percentage of votes each county contributed to the eleciton
+* Determine which country had the largest turnout.
+* Save the results to output file (election_results.txt)
+
+## Summary
+
+
